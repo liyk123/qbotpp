6f2a985b1dbb6d8daf291c875afc96c66ee6dc41
liyk123 <liyk__@outlook.com>
2026-05-29 18:07:48 +0800

    添加cmake与vcpkg支持并引入drogon

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Required for input/output operations, specifically std::cout
+#include <drogon/drogon.h>
+
+int main() 
+{
+    // Print "Hello, World!" to the console, followed by a newline character.
+    std::cout << "Hello, World!" << std::endl;
+    return 0; // Indicate successful execution of the program
+}
